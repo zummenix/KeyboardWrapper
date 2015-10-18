f3b556c881ca1ab720837255c00d25354c51946e
Aleksey Kuznetsov <zummenix@gmail.com>
2015-10-18 17:31:55 +0500

    Add more doc comments.

--- Pod/Classes/KeyboardWrapper.swift
+++ Pod/Classes/KeyboardWrapper.swift
@@ -1,17 +1,20 @@
 
 import UIKit
 
+/// Implement the method of this protocol to respond to `UIKeyboard` notifications.
 public protocol KeyboardWrapperDelegate: class {
+
+    /// Called when `KeyboardWrapper` will receive `UIKeyboard[WillShow|DidShow|WillHide|DidHide]Notification`.
     func keyboardWrapper(wrapper: KeyboardWrapper, didChangeWithKeyboardInfo info: KeyboardInfo)
 }
 
-/// Responsible for observing `UIKeyboard` notifications and calling delegate
-/// to notify about changes.
+/// Responsible for observing `UIKeyboard` notifications and calling `delegate` to notify about changes.
 public class KeyboardWrapper {
 
     /// The delegate for keyboard notifications.
     public weak var delegate: KeyboardWrapperDelegate?
 
+    /// Creates a new instance of `KeyboardWrapper` and adds itself as observer for `UIKeyboard` notifications.
     public init() {
         let center = NSNotificationCenter.defaultCenter()
         center.addObserver(self, selector: "keyboardWillShowNotification:", name: UIKeyboardWillShowNotification, object: nil)
@@ -20,6 +23,8 @@ public class KeyboardWrapper {
         center.addObserver(self, selector: "keyboardDidHideNotification:", name: UIKeyboardDidHideNotification, object: nil)
     }
 
+    /// Creates a new instance of `KeyboardWrapper`, adds itself as observer for `UIKeyboard` notifications and
+    /// sets `delegate`.
     public convenience init(delegate: KeyboardWrapperDelegate) {
         self.init()
         self.delegate = delegate
@@ -50,41 +55,49 @@ public class KeyboardWrapper {
     }
 }
 
-/// Represents keyboard state.
+/// Represents the keyboard state.
 public enum KeyboardState {
 
-    /// Denotes hidden state of keyboard.
+    /// Denotes hidden state of the keyboard.
+    /// Corresponds to `UIKeyboardDidHideNotification`.
     case Hidden
 
-    /// Denotes state when keyboard about to show.
+    /// Denotes state when the keyboard about to show.
+    /// Corresponds to `UIKeyboardWillShowNotification`.
     case WillShow
 
-    /// Denotes visible state of keyboard.
+    /// Denotes visible state of the keyboard.
+    /// Corresponds to `UIKeyboardDidShowNotification`.
     case Visible
 
-    /// Denotes state when keyboard about to hide.
+    /// Denotes state when the keyboard about to hide.
+    /// Corresponds to `UIKeyboardWillHideNotification`.
     case WillHide
 }
 
 /// Represents info about keyboard extracted from `NSNotification`.
 public struct KeyboardInfo {
 
-    /// The state of keyboard.
+    /// The state of the keyboard.
     public let state: KeyboardState
 
     /// The start frame of the keyboard in screen coordinates.
+    /// Corresponds to `UIKeyboardFrameBeginUserInfoKey`.
     public let beginFrame: CGRect
 
     /// The end frame of the keyboard in screen coordinates.
+    /// Corresponds to `UIKeyboardFrameEndUserInfoKey`.
     public let endFrame: CGRect
 
     /// Defines how the keyboard will be animated onto or off the screen.
+    /// Corresponds to `UIKeyboardAnimationCurveUserInfoKey`.
     public let animationCurve: UIViewAnimationCurve
 
     /// The duration of the animation in seconds.
+    /// Corresponds to `UIKeyboardAnimationDurationUserInfoKey`.
     public let animationDuration: NSTimeInterval
 
-    /// Options for animating constructed from `animationCurve`.
+    /// Options for animating constructed from `animationCurve` property.
     public var animationOptions: UIViewAnimationOptions {
         switch animationCurve {
         case .EaseInOut: return UIViewAnimationOptions.CurveEaseInOut
@@ -94,7 +107,7 @@ public struct KeyboardInfo {
         }
     }
 
-    /// Creates instance of `KeyboardInfo` using `userInfo` from `NSNotification` object and keyboard state.
+    /// Creates instance of `KeyboardInfo` using `userInfo` from `NSNotification` object and a keyboard state.
     /// If there is no info or `info` doesn't contain appropriate key-value pair uses default values.
     public static func fromNotificationUserInfo(info: [NSObject : AnyObject]?, state: KeyboardState) -> KeyboardInfo {
         var beginFrame = CGRectZero
