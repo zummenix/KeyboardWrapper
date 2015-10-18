775cb7c8e6b8b9377c3227657e8e352ce14fc13f
Aleksey Kuznetsov <zummenix@gmail.com>
2015-10-18 17:49:59 +0500

    Add unit tests.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,5 +3,37 @@ import XCTest
 import KeyboardWrapper
 
 class Tests: XCTestCase {
+
+    let keyboardWrapperDelegate = Delegate()
+    var keyboardWrapper: KeyboardWrapper?
+
+    override func setUp() {
+        super.setUp()
+        keyboardWrapper = KeyboardWrapper(delegate: keyboardWrapperDelegate)
+    }
+
+    func testKeyboardStates() {
+        let notificationNames = [
+            UIKeyboardWillShowNotification,
+            UIKeyboardDidShowNotification,
+            UIKeyboardWillHideNotification,
+            UIKeyboardDidHideNotification
+        ]
+
+        for name in notificationNames {
+            NSNotificationCenter.defaultCenter().postNotificationName(name, object: nil)
+        }
+
+        XCTAssertEqual(keyboardWrapperDelegate.states, [.WillShow, .Visible, .WillHide, .Hidden])
+    }
     
+}
+
+class Delegate: KeyboardWrapperDelegate {
+
+    var states = [KeyboardState]()
+
+    func keyboardWrapper(wrapper: KeyboardWrapper, didChangeKeyboardInfo info: KeyboardInfo) {
+        states.append(info.state)
+    }
 }
